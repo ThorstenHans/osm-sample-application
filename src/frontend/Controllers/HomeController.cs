5e20664c8a8d3c92471cb59c00aa70547c1d2ab5
Thorsten Hans <thorsten.hans@gmail.com>
2020-08-21 14:32:10 +0200

    update sample frontend

--- src/frontend/Controllers/HomeController.cs
+++ src/frontend/Controllers/HomeController.cs
@@ -41,11 +41,6 @@ namespace SampleFrontend.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
