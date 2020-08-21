4326481e6dd7ed0558fdbdfd85487b9483325376
Thorsten Hans <thorsten.hans@gmail.com>
2020-08-21 14:21:32 +0200

    update frontend

--- src/frontend/Controllers/HomeController.cs
+++ src/frontend/Controllers/HomeController.cs
@@ -9,37 +9,47 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using SampleFrontend.Models;
 
-namespace SampleFrontend.Controllers {
-    public class HomeController : Controller {
+namespace SampleFrontend.Controllers
+{
+    public class HomeController : Controller
+    {
         private readonly ILogger<HomeController> _logger;
 
         public IHttpClientFactory ClientFactory { get; }
         public IConfiguration Configuration { get; }
 
-        public HomeController (IHttpClientFactory clientFactory, IConfiguration configuration, ILogger<HomeController> logger) {
+        public HomeController(IHttpClientFactory clientFactory, IConfiguration configuration, ILogger<HomeController> logger)
+        {
             ClientFactory = clientFactory;
             Configuration = configuration;
             _logger = logger;
         }
 
-        public IActionResult Index () {
-            var requestUrl = Configuration.GetValue<string> ("API_URL");
-            var request = new HttpRequestMessage (HttpMethod.Get, requestUrl);
+        public IActionResult Index()
+        {
+            var requestUrl = Configuration.GetValue<string>("API_URL");
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
 
-            var client = ClientFactory.CreateClient ();
-            var response = client.SendAsync (request).GetAwaiter ().GetResult ();
-            var body = response.Content.ReadAsStringAsync ().GetAwaiter ().GetResult ();
-            ViewData["Response"] = body;
-            return View ();
+            var client = ClientFactory.CreateClient();
+            var response = client.SendAsync(request).GetAwaiter().GetResult();
+            string responseBody = "Error while reading data from the API";
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            }
+            ViewData["Response"] = responseBody;
+            return View();
         }
 
-        public IActionResult Privacy () {
-            return View ();
+        public IActionResult Privacy()
+        {
+            return View();
         }
 
-        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error () {
-            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
